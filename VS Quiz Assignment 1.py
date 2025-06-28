9c8bdc1835973949601e609156fe1bce1acb3da6
AI-HUB-OFFICIAL <aihub.nsk@gmail.com>
2025-06-29 00:11:42 +0500

    Add files via upload

---
+++ VS Quiz Assignment 1.py
@@ -0,0 +1,35 @@
+#1. Print your name using the print() function.
+print("Nehan Mustafa Khan")
+#2. Create a variable for your age and print it
+my_age = 16
+print(my_age)
+#3. Store your school or institute name in a variable and print it.
+school_name = "The Educators GAC"
+print(school_name)
+#4. Add two numbers using variables and print the result.
+a = 3
+b = 4
+c = a + b
+print(c)
+#5. Store your name in a variable and print a welcome message.
+my_name = "Nehan"
+print("Welcome,",my_name,"!")
+#6. Declare a variable for your city and print where you live.
+my_city = "Gujranwala"
+print(my_city)
+#7. Use multiple variables (name, age, city) and print a full sentence.
+name = "Nehan"
+age = 16
+city = "Gujranawala"
+print(f"My name is {name}. I am {age} years old. I live in {city}")
+#8. Perform subtraction between two numbers using variables and print the answer.
+e = 5
+f = 3
+g = e - f
+print(g)
+#9. Store a motivational quote in a variable and print it.
+quote = "Failure is the word unknown to me."
+print(quote)
+#10. Create a variable for your favorite subject and print it.
+f_subject = "Math"
+print(f_subject)
