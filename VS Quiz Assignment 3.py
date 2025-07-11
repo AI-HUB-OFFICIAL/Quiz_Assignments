e9990817bee2d6c85db548e07e656dcee4acf839
AI-HUB-OFFICIAL <aihub.nsk@gmail.com>
2025-07-12 00:20:22 +0500

    Add files via upload

---
+++ VS Quiz Assignment 3.py
@@ -0,0 +1,64 @@
+#1. Declare a variable student_id with value 101 (integer) and a variable grades with a list: [85, 90, 92].
+#Print the value and type of both variables using the type() function.
+student_id = 101
+grades = [85, 90, 92]
+print(student_id, type(student_id))
+print(grades, type(grades))
+#2.  Create a variable price with value 19.99 (float) and a variable dimensions with a tuple: (5.5, 3.2).
+#Print the value and type of both variables.
+price = 19.99
+dimensions = (5.5, 3.2)
+print(price, type(price))
+print(dimensions, type(dimensions))
+#3. Declare a variable complex_value with a complex number 2 + 3j, and a variable numbers with range(1, 5).
+#Print the value and type of both variables.
+complex_value = 2 + 3j
+numbers = range(1, 5)
+print(complex_value, type(complex_value))
+print(numbers, type(numbers))
+#4. Create a variable is_active with boolean value True, and a variable unique_ids with a set: {1, 2, 3}.
+#Print the value and type of both variables.
+is_active = True
+unique_ids = {1, 2, 3}
+print(is_active, type(is_active))
+print(unique_ids, type(unique_ids))
+#5. Declare a variable status with a string 'Active' using single quotes, and a variable fixed_colors with a frozenset containing "red" and "blue".
+#Print the value and type of both variables.
+status = 'Active'
+fixed_colors = frozenset(["red", "blue"])
+print(status, type(status))
+print(fixed_colors, type(fixed_colors))
+#6.  Create a variable country with a string "Canada" using double quotes, and a variable cities with a list: ["Toronto", "Vancouver"].
+#Print the value and type of both variables.
+country = "Canada"
+cities = ["Toronto", "Vancover"]
+print(country, type(country))
+print(cities, type(cities))
+#7.  Declare a variable motto with the string 'Keep it simple' using triple single quotes, and a variable coordinates with a tuple: (10, 20, 30).
+#Print the value and type of both variables.
+motto = '''Keep it simple'''
+coordinates = (10, 20, 30)
+print(motto, type(motto))
+print(coordinates, type(coordinates))
+#8. Create a variable quantity with value 50 (integer), and a variable tags with a set: {"urgent", "new"}.
+#Print the value and type of both variables.
+quantity = 50
+tags = {"urgent", "new"}
+print(quantity, type(quantity))
+print(tags, type(tags))
+#9. Declare a variable distance with a float value 42.5, and a variable steps with range(0, 10, 2).
+#Print the value and type of both variables.
+distance = 42.5
+steps = range(0, 10, 2)
+print(distance, type(distance))
+print(steps, type(steps))
+#10.**Create a variable note with a multi-line string using triple double quotes:
+#Meeting at 9 AM  
+#Bring notebook
+#and a variable locked_values with a frozenset: {100, 200, 300}.
+#Print the value and type of both variables.**
+note = """Meeting at 9 AM
+Bring notebook"""
+locked_values = frozenset([100, 200, 300])
+print(note, type(note))
+print(locked_values, type(locked_values))
